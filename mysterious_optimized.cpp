c564e255dd40f8d6b5794c8a9593542ec6fb16dd
Suraj Patil <srpatil@uci.edu>
2020-03-19 22:24:04 +0530

    Replace cin,cout & endl with printf,scanf & \n to speed up program

---
+++ mysterious_optimized.cpp
@@ -0,0 +1,127 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+/*** author: https://github.com/Waqar-107 ***/
+
+#include<algorithm>
+#include<cctype>
+#include<cmath>
+#include<cstdio>
+#include<cstdlib>
+#include<cstring>
+#include<functional>
+#include<iomanip>
+#include<iostream>
+#include<list>
+#include<map>
+#include<numeric>
+#include<queue>
+#include<set>
+#include<stack>
+#include<string>
+#include<utility>
+#include<vector>
+
+#define N 5010
+
+using namespace std;
+
+struct env
+{
+	int w, h, idx;
+	env() {}
+	env(int w, int h, int idx) {
+		this->w = w;
+		this->h = h;
+		this->idx = idx;
+	}
+};
+
+bool cmp(env a, env b) {
+	if (a.w == b.w)
+		return a.h < b.h;
+
+	return a.w < b.w;
+}
+
+int w, h;
+vector<env> vec;
+int dp[N];
+int parent[N];
+
+int main()
+{
+	ios_base::sync_with_stdio(0); // This speeds up the program
+	
+  	int i, j, k;
+	int n, m;
+
+ 	scanf("%d %d %d",&n,&w,&h);
+
+	for (i = 1; i <= n; i++)
+	{
+
+		scanf("%d %d",&j,&k);
+
+		if (j > w && k > h)
+			vec.push_back(env(j, k, i));
+	}
+
+	if (vec.size() == 0) {
+
+		printf("0\n");
+		return 0;
+	}
+
+	sort(vec.begin(), vec.end(), cmp);
+
+	n = vec.size();
+
+	memset(dp, 0, sizeof(dp));
+	memset(parent, -1, sizeof(parent));
+
+	dp[0] = 1;
+	for (i = 1; i < n; i++)
+	{
+		for (j = i - 1; j >= 0; j--)
+		{
+			if (vec[j].w < vec[i].w && vec[j].h < vec[i].h)
+			{
+				if (1 + dp[j] > dp[i])
+				{
+					dp[i] = 1 + dp[j];
+					parent[i] = j;
+				}
+			}
+		}
+
+		if (!dp[i])dp[i] = 1;
+	}
+
+	int mx = 1; k = 0;
+	for (i = 1; i < n; i++)
+	{
+		if (dp[i] > mx)
+			mx = dp[i], k = i;
+	}
+
+
+	// endl forces us to empty the buffer stream which is unnecessary
+
+	printf("%d\n",mx);
+
+	vector<int> ans; ans.push_back(vec[k].idx);
+	while (1)
+	{
+		k = parent[k];
+		if (k == -1)break;
+
+		ans.push_back(vec[k].idx);
+	}
+
+	reverse(ans.begin(), ans.end());
+	for (int e : ans)
+
+		printf("%d\n",e);
+
+	return 0;
+}
